c087f4710f8f3e6c7879504f331b75ea9e165d26
tirzasrwn <tirzasrwn@gmail.com>
2023-05-13 13:41:12 +0700

    feat: Add more git alias.

--- fish/conf.d/user_config.fish
+++ fish/conf.d/user_config.fish
@@ -1,11 +1,18 @@
 # Disable greeting message
 set fish_greeting ""
 
+# Set alias
+alias psx "ps xf | less -S"
+
 if type -q exa
   alias ll "exa -l -g --icons"
   alias la "ll -a"
   alias lt "ll --tree --level=2"
-  alias psx "ps xf | less -S"
+end
+
+if type -q git
   alias gl="git log --all --decorate --oneline --graph"
+  alias gst="git status"
+  alias gd="git diff"
 end
 
