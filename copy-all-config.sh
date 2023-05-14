7b7666289fe8be0cde3a96012ffb420fa15a64cd
tirzasrwn <tirzasrwn@gmail.com>
2023-05-14 20:02:03 +0700

    fix: Create '.config' directory for '~/.config' directory.

--- copy-all-config.sh
+++ copy-all-config.sh
@@ -2,12 +2,12 @@
 
 CONFIG_DIR="$HOME/.config"
 
-cp -frv $CONFIG_DIR/dunst ./
-cp -frv $CONFIG_DIR/fish/conf.d/readme.md ./fish/conf.d/
-cp -frv $CONFIG_DIR/fish/conf.d/user_config.fish ./fish/conf.d/
-cp -frv $CONFIG_DIR/i3 ./
-cp -frv $CONFIG_DIR/kitty ./
-cp -frv $CONFIG_DIR/polybar ./
-cp -frv $CONFIG_DIR/rofi ./
-cp -frv $CONFIG_DIR/screenkey.json ./
+cp -frv $CONFIG_DIR/dunst ./.config/
+cp -frv $CONFIG_DIR/fish/conf.d/readme.md ./fish/conf.d/.config/
+cp -frv $CONFIG_DIR/fish/conf.d/user_config.fish ./fish/conf.d/.config/
+cp -frv $CONFIG_DIR/i3 ./.config/
+cp -frv $CONFIG_DIR/kitty ./.config/
+cp -frv $CONFIG_DIR/polybar ./.config/
+cp -frv $CONFIG_DIR/rofi ./.config/
+cp -frv $CONFIG_DIR/screenkey.json ./.config/
 
