267173e913848e9d1fff06dd30dbf53b112bc5f6
tirzasrwn <tirzasrwn@gmail.com>
2023-05-17 15:39:52 +0700

    fix: Copying fish config directory.

--- copy-all-config.sh
+++ copy-all-config.sh
@@ -3,8 +3,8 @@
 CONFIG_DIR="$HOME/.config"
 
 cp -frv $CONFIG_DIR/dunst ./.config/
-cp -frv $CONFIG_DIR/fish/conf.d/readme.md ./fish/conf.d/.config/
-cp -frv $CONFIG_DIR/fish/conf.d/user_config.fish ./fish/conf.d/.config/
+cp -frv $CONFIG_DIR/fish/conf.d/readme.md ./.config/fish/conf.d/
+cp -frv $CONFIG_DIR/fish/conf.d/user_config.fish ./.config/fish/conf.d/
 cp -frv $CONFIG_DIR/i3 ./.config/
 cp -frv $CONFIG_DIR/kitty ./.config/
 cp -frv $CONFIG_DIR/polybar ./.config/
