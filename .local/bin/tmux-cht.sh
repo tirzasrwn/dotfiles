4691b25cb0740fe2df53569cde51d577552b1e38
tirzasrwn <tirzasrwn@gmail.com>
2025-04-01 10:24:59 +0700

    feat: Add ch.sh command list and change tmux implementation.

--- .local/bin/tmux-cht.sh
+++ .local/bin/tmux-cht.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-#
+
+# Path to the cht.sh script
 SCRIPT_PATH="/tmp/cht.sh"
 
-# Download cht.sh script if not exists
+# Download cht.sh script if it doesn't exist
 if [ ! -f "$SCRIPT_PATH" ]; then
 	curl -sSL https://cht.sh/:cht.sh >"$SCRIPT_PATH"
 fi
 
-read -p "enter query: " query
-tmux neww bash -c "bash $SCRIPT_PATH $query | less -SR"
+# Read the list from the specified file
+LIST="$HOME/.local/bin/tmux-cht-list"
+
+# Select a language using fzf
+selected=$(cat "$LIST" | fzf --print-query --prompt="ch.sh/" | tail -1)
+if [[ -z $selected ]]; then
+	exit 0
+fi
+
+# Format the query for the URL
+query=$(echo "$query" | tr ' ' '+')
+
+# Open a new tmux window and execute the command
+tmux neww bash -c "bash $SCRIPT_PATH $selected | less -SR"
